ea962bc1c02c0f3d0a4cbf5dcf4976577381ec11
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-20 02:47:00 +0700

    Create getid.py

---
+++ plugins/getid.py
@@ -0,0 +1,74 @@
+"""Get id of the replied user
+Syntax: /id"""
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.types.messages_and_media import message
+
+
+@Bot.on_message(filters.command('id') & filters.private & subscribed)
+async def showid(client, message):
+    chat_type = message.chat.type
+
+    if chat_type == "private":
+        user_id = message.chat.id
+        await message.reply_text(
+            f"<code>{user_id}</code>",
+            quote=True
+        )
+
+    elif chat_type in ["group", "supergroup"]:
+        _id = ""
+        _id += (
+            "<b>👥 Chat ID</b>: "
+            f"<code>{message.chat.id}</code>\n"
+        )
+        if message.reply_to_message:
+            _id += (
+                "<b>🙋‍♂️ Replied User ID</b>: "
+                f"<code>{message.reply_to_message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        else:
+            _id += (
+                "<b>👤 User ID</b>: "
+                f"<code>{message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        await message.reply_text(
+            _id,
+            quote=True
+        )
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
