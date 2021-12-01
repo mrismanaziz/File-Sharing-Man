c0858fdccac0f14bcf79d418c5be462107d3002b
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-01 15:24:00 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,12 +11,22 @@ from config import CHANNEL, GROUP, OWNER
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+    if query.startswith("home"):
+        if query == 'home':
+            chat_id = query.from_user.id
+            message_id = query.message.message_id
+            await query.message.delete()
+            await query.message.edit_text(
+                chat_id=chat_id,
+                text=Data.START.format(callback_query.from_user.mention, mention),
+                reply_markup=InlineKeyboardMarkup(Data.buttons),
+            )
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n• Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
+                [[InlineKeyboardButton("ᴋᴇᴍʙᴀʟɪ", callback_data="home")]]
             ),
         )
     elif data == "close":
