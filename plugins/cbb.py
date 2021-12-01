0612c265bdc72ce080b42d7bf41ce047b0025b84
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-01 15:15:40 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>• Owner: @{OWNER}</a>\n• Channel: @{CHANNEL}\n• Group: @{GROUP}</b>\n<b>• Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a></b>\n",
+            text=f"<b>• Owner: @{OWNER}\n• Channel: @{CHANNEL}\n• Group: @{GROUP}\n• Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
