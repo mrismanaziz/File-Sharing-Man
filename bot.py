99ae713c51246efbcfcf4c00ee08e2442a4bf5df
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-01 15:19:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
         )
         self.username = usr_bot_me.username
 
