b5cdb0263a6179827d001038218575daf8bb08ee
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-01 16:18:26 +0700

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -41,8 +41,6 @@ async def add_user(id, user_name):
             usr = Broadcast(id, user_name)
             SESSION.add(usr)
             SESSION.commit()
-        else:
-            pass
 
 
 async def full_userbase():
@@ -53,7 +51,6 @@ async def full_userbase():
 
 async def query_msg():
     try:
-        query = SESSION.query(Broadcast.id).order_by(Broadcast.id)
-        return query
+        return SESSION.query(Broadcast.id).order_by(Broadcast.id)
     finally:
         SESSION.close()
