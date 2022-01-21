12761b97ae40599c5293e90adc01849f7e8e5709
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-21 07:06:06 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -115,8 +115,9 @@ async def start_command(client: Client, message: Message):
         buttons = [
             [InlineKeyboardButton("• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about")],
             [
-                InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitelink),
-                InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣", url=client.invitelink2),
+                InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitrlink),
+                InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitelink2),
+                InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣", url=client.invitelink3),
             ],
             [
                 InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close"),
@@ -145,7 +146,8 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitelink),
-            InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣", url=client.invitelink2),
+            InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitelink2),
+            InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣", url=client.invitelink3),
         ],
     ]
     try:
