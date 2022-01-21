a25e1e69dacd568738d591edf2b7de024df5191f
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-21 12:02:58 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -21,7 +21,7 @@ from helper_func import encode
     )
 )
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
+    reply_text = await message.reply_text("<code>Tunggu Bentar bree...</code>", quote=True)
     try:
         post_message = await message.copy(
             chat_id=client.db_channel.id, disable_notification=True
