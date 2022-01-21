178157f1242e8e8ca36d3422093683fc051e55b0
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-21 07:12:18 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -57,7 +57,7 @@ async def batch(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                    "🔥Share Link🔥", url=f"https://telegram.me/share/url?url={link}"
                 )
             ]
         ]
@@ -96,7 +96,7 @@ async def link_generator(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                    "🔥Share Link🔥", url=f"https://telegram.me/share/url?url={link}"
                 )
             ]
         ]
