97c524cde6d84e8e9ceca821697eb716f163e01a
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-24 06:51:59 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -28,8 +28,6 @@ async def is_subscribed(filter, client, update):
         member = await client.get_chat_member(
             chat_id=FORCE_SUB_CHANNEL1, user_id=user_id
         )
-
-
         member = await client.get_chat_member(
             chat_id=FORCE_SUB_CHANNEL2, user_id=user_id
         )
