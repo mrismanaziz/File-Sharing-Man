396aa16ec87fb68a6f7f93616620bae0e02e0629
mrismanaziz <mrismanaziz@gmail.com>
2022-01-25 02:49:06 +0700

    add support deployed on vps and clean up startup

--- config.py
+++ config.py
@@ -4,7 +4,10 @@
 
 import logging
 import os
+from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
+
+load_dotenv("config.env")
 
 # Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
