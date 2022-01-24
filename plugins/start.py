e93606be535351763f1fc52f785bacd064e09473
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-24 07:34:55 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -117,7 +117,7 @@ async def start_command(client: Client, message: Message):
             [                
                 InlineKeyboardButton("💠𝗖𝗛𝗔𝗡𝗡𝗘𝗟💠", url=client.invitelink1),
                 InlineKeyboardButton("💠𝗖𝗛𝗔𝗡𝗡𝗘𝗟💠", url=client.invitelink2),
-                InlineKeyboardButton("💠𝗚𝗥𝗢𝗨𝗣💠", url=client.invitelink),
+                InlineKeyboardButton("💠𝗚𝗥𝗢𝗨𝗣💠", url=client.invitelink3),
             ],                         
             [
                 InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close"),
@@ -147,7 +147,7 @@ async def not_joined(client: Client, message: Message):
         [            
             InlineKeyboardButton("💠𝗖𝗛𝗔𝗡𝗡𝗘𝗟💠", url=client.invitelink1),
             InlineKeyboardButton("💠𝗖𝗛𝗔𝗡𝗡𝗘𝗟💠", url=client.invitelink2),
-            InlineKeyboardButton("💠𝗚𝗥𝗢𝗨𝗣💠", url=client.invitelink),
+            InlineKeyboardButton("💠𝗚𝗥𝗢𝗨𝗣💠", url=client.invitelink3),
         ],
                   
     ]
