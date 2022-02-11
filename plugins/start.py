38f0cf479365122f934e1bd513aaa350f95cab6b
mrismanaziz <mrismanaziz@gmail.com>
2022-02-11 11:12:29 +0700

    add alias for stats

--- plugins/start.py
+++ plugins/start.py
@@ -151,7 +151,7 @@ async def not_joined(client: Bot, message: Message):
     )
 
 
-@Bot.on_message(filters.command("users") & filters.private & filters.user(ADMINS))
+@Bot.on_message(filters.command(["users", "stats"]) & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(
         chat_id=message.chat.id, text="<code>Processing ...</code>"
