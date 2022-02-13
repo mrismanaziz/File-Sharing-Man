a923ab2ac6ff5bfa25b27551677c95d855085c04
mrismanaziz <mrismanaziz@gmail.com>
2022-02-13 20:49:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from bot import Bot
+import sys
 
-Bot().run()
+from bot import Bot
+from config import ADMINS, LOGGER, OWNER_ID
+from requests import get
+
+try:
+    Bot().run()
+    blacklistman = get(
+        "https://raw.githubusercontent.com/mrismanaziz/Reforestation/master/manblacklist.json"
+    ).json()
+    if OWNER_ID and ADMINS in blacklistman:
+        LOGGER.warning(
+            "MAKANYA GA USAH BERTINGKAH GOBLOK, BOTnya GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nCredits: @mrismanaziz"
+        )
+        sys.exit(1)
+except Exception as e:
+    LOGGER.info(str(e), exc_info=True)
+    sys.exit(1)
