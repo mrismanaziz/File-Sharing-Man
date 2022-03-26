6f1ad0dfeaee1bd5d0c5eabfdba782c6d9fe73a9
senseiubot <100754362+senseiubot@users.noreply.github.com>
2022-03-26 07:23:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -37,6 +37,9 @@ GROUP = os.environ.get("GROUP", "SharingUserbot")
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
