db388b42534b6ae26444c961f909d5a0bb24ad15
mrismanaziz <mrismanaziz@gmail.com>
2022-05-22 14:44:57 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -61,7 +61,7 @@ async def channel_post(client: Client, message: Message):
 
 
 @Bot.on_message(
-    filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited
+    filters.channel & filters.incoming & filters.chat(CHANNEL_ID)
 )
 async def new_post(client: Client, message: Message):
 
