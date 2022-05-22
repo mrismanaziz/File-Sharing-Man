65ef3156b623c037c9cb706ea825403f74a2e03e
mrismanaziz <mrismanaziz@gmail.com>
2022-05-22 16:33:17 +0700

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,54 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+<b> ❏ Perintah untuk Pengguna BOT
+ ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
+ ├ /help - Bantuan Perintah Bot ini
+ ├ /ping - Untuk mengecek bot hidup
+ └ /uptime - Untuk melihat status bot 
+ 
+ ❏ Perintah Untuk Admin BOT
+ ├ /logs - Untuk melihat logs bot
+ ├ /vars - Untuk melihat variable bot
+ ├ /setvar - Untuk mengatur var dengan command dibot
+ ├ /delvar - Untuk menghapus var dengan command dibot
+ ├ /getvar - Untuk melihat salah satu var dengan command dibot
+ ├ /users - Untuk melihat statistik pengguna bot
+ ├ /batch - Untuk membuat link lebih dari satu file
+ ├ /speedtest - Untuk Mengetes kecepatan server bot
+ └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+👨‍💻 Develoved by @Lunatic0de</b>
+"""
+
+    close = [
+        [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
+    ]
+
+    mbuttons = [
+        [
+            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    buttons = [
+        [
+            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    ABOUT = """
+<b>Tentang Bot ini:
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+ • Creator: @{}
+ • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
+ • Owner Repo: @mrismanaziz
+👨‍💻 Develoved by @Lunatic0de</b>
+"""
