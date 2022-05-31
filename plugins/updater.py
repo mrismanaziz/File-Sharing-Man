7ce0797ad4bb17ad8206c4f9e561643e7a6562ec
mrismanaziz <mrismanaziz@gmail.com>
2022-05-31 18:06:58 +0700

    Create updater.py

---
+++ plugins/updater.py
@@ -0,0 +1,84 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import os
+import sys
+from os import environ, execle, system
+
+from git import Repo
+from git.exc import InvalidGitRepositoryError
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from main import LOGGER
+
+UPSTREAM_REPO = "https://github.com/mrismanaziz/File-Sharing-Man"
+
+
+def gen_chlog(repo, diff):
+    upstream_repo_url = Repo().remotes[0].config_reader.get("url").replace(".git", "")
+    ac_br = repo.active_branch.name
+    ch_log = ""
+    tldr_log = ""
+    ch = f"<b>updates for <a href={upstream_repo_url}/tree/{ac_br}>[{ac_br}]</a>:</b>"
+    ch_tl = f"updates for {ac_br}:"
+    d_form = "%d/%m/%y || %H:%M"
+    for c in repo.iter_commits(diff):
+        ch_log += (
+            f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b>"
+            f"<a href={upstream_repo_url.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
+        )
+        tldr_log += f"\n\n💬 {c.count()} 🗓 [{c.committed_datetime.strftime(d_form)}]\n[{c.summary}] 👨‍💻 {c.author}"
+    if ch_log:
+        return str(ch + ch_log), str(ch_tl + tldr_log)
+    return ch_log, tldr_log
+
+
+def updater():
+    try:
+        repo = Repo()
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", UPSTREAM_REPO)
+        origin.fetch()
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+    ac_br = repo.active_branch.name
+    if "upstream" in repo.remotes:
+        ups_rem = repo.remote("upstream")
+    else:
+        ups_rem = repo.create_remote("upstream", UPSTREAM_REPO)
+    ups_rem.fetch(ac_br)
+    changelog, tl_chnglog = gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+    return bool(changelog)
+
+
+
+@Bot.on_message(filters.command("update") & filters.user(ADMINS))
+async def update_bot(_, message: Message):
+    message.chat.id
+    msg = await message.reply_text("Checking updates...")
+    update_avail = updater()
+    if update_avail:
+        await msg.edit("✅ Update finished !")
+        system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
+        execle(sys.executable, sys.executable, "main.py", environ)
+        return
+    await msg.edit(
+        f"Bot is **up-to-date** with branch [master]({UPSTREAM_REPO}/tree/master)",
+        disable_web_page_preview=True,
+    )
+
+
+@Bot.on_message(filters.command("restart") & filters.user(ADMINS))
+async def restart_bot(_, message: Message):
+    try:
+        msg = await message.reply_text("`Restarting bot...`")
+        LOGGER(__name__).info("BOT SERVER RESTARTED !!")
+    except BaseException as err:
+        LOGGER(__name__).info(f"{err}")
+        return
+    await msg.edit_text("✅ Bot has restarted !\n\n")
+    os.system(f"kill -9 {os.getpid()} && bash start")
