9f62bbbb1083bdc41f7286ad63c2d9c91894c6b7
Sourcery AI <>
2023-06-09 10:25:26 +0000

    'Refactored by Sourcery'

--- helper_func.py
+++ helper_func.py
@@ -69,15 +69,13 @@ async def is_subscribed(filter, client, update):
 async def encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(string_bytes)
-    base64_string = (base64_bytes.decode("ascii")).strip("=")
-    return base64_string
+    return (base64_bytes.decode("ascii")).strip("=")
 
 async def decode(base64_string):
     base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string = string_bytes.decode("ascii")
-    return string
+    string_bytes = base64.urlsafe_b64decode(base64_bytes)
+    return string_bytes.decode("ascii")
 
 
 async def get_messages(client, message_ids):
@@ -114,8 +112,8 @@ async def get_message_id(client, message):
         matches = re.match(pattern, message.text)
         if not matches:
             return 0
-        channel_id = matches.group(1)
-        msg_id = int(matches.group(2))
+        channel_id = matches[1]
+        msg_id = int(matches[2])
         if channel_id.isdigit():
             if f"-100{channel_id}" == str(client.db_channel.id):
                 return msg_id
